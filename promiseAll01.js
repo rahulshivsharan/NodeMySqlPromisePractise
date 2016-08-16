45d9da25b256448c57167b2adc0df7224a9dd4a5
Rahul Shivsharan <rahul.shivsharan@ril.com>
2016-08-16 16:40:40 +0530

    Executing Promise parallely using Promise.All
    Promise.All executes promises in bunch,
    the example shows the same conecept.
    
    There are individual promise execution of select query

---
+++ promiseAll01.js
@@ -0,0 +1,49 @@
+var Promise = require("promise");
+var mySql = require("mysql");
+var _ = require("underscore");
+
+var connection = mySql.createConnection({
+	host : "localhost",
+	user : "root",
+	password : "rahul",
+	database : "testDb" //schema
+});
+
+connection.connect();
+
+function getDepartment(departmentId){
+	return new Promise(fn);
+
+	function fn(resolve,reject){
+		var sql = "select * from departmentTbl where deptid = "+departmentId;
+		connection.query(sql,function(err,rows,fields){
+			if(err){								
+				return reject(err);
+			}else{				
+				return resolve(rows);
+			}
+		});
+	}
+}
+
+function getSelectedDept(){
+	var promises = [];
+	if(arguments.length > 0){
+		promises = arguments;
+	}else{
+		throw new Error("Accepts Promise as arguments");
+	}
+
+	return Promise.all(promises);
+}
+
+getSelectedDept(getDepartment(16),getDepartment(19),getDepartment(17)).then(function(rows){
+	_.each(rows,function(row){
+		var department = row[0];
+		console.log("Department Id \""+department.deptId+"\", DepartmentName \""+department.deptName+"\"");	
+	});	
+}).catch(function(err){
+	console.log(err);
+}).done(function(){
+	connection.end();
+});
